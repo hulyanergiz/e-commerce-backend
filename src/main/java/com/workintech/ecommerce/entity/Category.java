e81c954134f7450e746f010c6c2a8b8a8677140a
Hülya Nergiz <akin.hulya@outlook.com>
2024-02-29 22:24:05 +0300

    creates Category entity

---
+++ src/main/java/com/workintech/ecommerce/entity/Category.java
@@ -0,0 +1,31 @@
+package com.workintech.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="category",schema="ecommerce")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String img;
+
+    private Double rating;
+
+    private String gender;
+
+    @OneToMany(mappedBy ="category",cascade = CascadeType.ALL)
+    private List<Product> products;
+
+}
