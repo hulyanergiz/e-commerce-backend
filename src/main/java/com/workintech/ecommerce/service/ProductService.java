c3f6b078fafe00e01c43712a7a16f70b100a3273
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-01 18:02:17 +0300

    creates ProductService

---
+++ src/main/java/com/workintech/ecommerce/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.workintech.ecommerce.service;
+
+import com.workintech.ecommerce.dto.ProductResponse;
+import com.workintech.ecommerce.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<ProductResponse> findAll();
+
+    Product findById(Long id);
+
+    ProductResponse getById(Long id);
+
+    ProductResponse save(Product product);
+
+    ProductResponse update(Long id,Product product);
+
+    ProductResponse delete(Long id);
+
+    //TODO: Add more find methods for different parameters
+}
