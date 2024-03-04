48f352589ef3ed076a46d124c978c9d09267ea92
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-04 17:50:26 +0300

    ProductController

---
+++ src/main/java/com/workintech/ecommerce/controller/ProductController.java
@@ -0,0 +1,43 @@
+package com.workintech.ecommerce.controller;
+
+import com.workintech.ecommerce.dto.ProductResponse;
+import com.workintech.ecommerce.entity.Product;
+import com.workintech.ecommerce.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public List<ProductResponse> findAll(){
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponse getById(@PathVariable Long id){
+        return productService.getById(id);
+    }
+
+    @PostMapping("/categoryId/{categoryId}")
+    public ProductResponse save(@PathVariable Long categoryId,@RequestBody Product product){
+        return productService.save(categoryId,product);
+    }
+
+    @PutMapping("/{id}")
+    public ProductResponse update(@PathVariable Long id,@RequestBody Product product){
+        return productService.update(id,product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ProductResponse delete(@PathVariable Long id){
+        return productService.delete(id);
+    }
+
+}
