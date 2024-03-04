573eb09eb1b4c3654056c3721f2d837e1502ee68
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-04 17:47:15 +0300

    creates EcommerceException

---
+++ src/main/java/com/workintech/ecommerce/exception/EcommerceException.java
@@ -0,0 +1,16 @@
+package com.workintech.ecommerce.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class EcommerceException extends RuntimeException{
+
+    private HttpStatus httpStatus;
+
+    public EcommerceException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+
+    }
+}
