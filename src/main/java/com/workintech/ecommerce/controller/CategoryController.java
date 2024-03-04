065dbbed5ef6abcdf521ef8391fa3cba70b71304
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-04 17:48:44 +0300

    CategoryController

---
+++ src/main/java/com/workintech/ecommerce/controller/CategoryController.java
@@ -0,0 +1,42 @@
+package com.workintech.ecommerce.controller;
+
+import com.workintech.ecommerce.dto.CategoryResponse;
+import com.workintech.ecommerce.entity.Category;
+import com.workintech.ecommerce.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public List<CategoryResponse> findAll(){
+        return categoryService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public CategoryResponse getById(@PathVariable Long id){
+        return categoryService.getById(id);
+    }
+
+    @PostMapping
+    public CategoryResponse save(@RequestBody Category category){
+        return categoryService.save(category);
+    }
+
+    @PutMapping("/{id}")
+    public CategoryResponse update(@PathVariable Long id, @RequestBody Category category){
+        return categoryService.update(id,category);
+    }
+
+    @DeleteMapping("/{id}")
+    public CategoryResponse delete(@PathVariable Long id){
+        return categoryService.delete(id);
+    }
+}
