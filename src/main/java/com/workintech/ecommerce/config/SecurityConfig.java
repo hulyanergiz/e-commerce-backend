3dc571551b366447bb769a046eac3650adac2532
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-06 00:06:12 +0300

    adds necessary security beans to use for register method

--- src/main/java/com/workintech/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/workintech/ecommerce/config/SecurityConfig.java
@@ -3,13 +3,31 @@ package com.workintech.ecommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(UserDetailsService userDetailsService){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(provider);
+    }
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf((csrf) -> csrf.disable()).authorizeHttpRequests(auth -> {
@@ -23,6 +41,17 @@ public class SecurityConfig {
             auth.requestMatchers(HttpMethod.POST, "/products/**").permitAll();
             auth.requestMatchers(HttpMethod.PUT,"/products/**").permitAll();
             auth.requestMatchers(HttpMethod.DELETE,"/products/**").permitAll();
+            auth.requestMatchers(HttpMethod.GET, "/roles/**").permitAll();
+            auth.requestMatchers(HttpMethod.POST, "/roles/**").permitAll();
+            auth.requestMatchers(HttpMethod.PUT, "/roles/**").permitAll();
+            auth.requestMatchers(HttpMethod.DELETE, "/roles/**").permitAll();
+            auth.requestMatchers(HttpMethod.POST, "/users/**").permitAll();
+            auth.requestMatchers(HttpMethod.DELETE, "/users/**").permitAll();
+            auth.requestMatchers(HttpMethod.GET, "/store/**").permitAll();
+            auth.requestMatchers(HttpMethod.POST, "/store/**").hasAuthority("ADMIN");
+            auth.requestMatchers(HttpMethod.PUT, "/store/**").permitAll();
+            auth.requestMatchers(HttpMethod.DELETE, "/store/**").permitAll();
+            auth.requestMatchers("/signup/**").permitAll();
             auth.anyRequest().authenticated();
 
         }).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults()).build();
