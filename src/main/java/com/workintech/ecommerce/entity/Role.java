16dcf1193f1cd8570e2da1d6c17d1a5eefa3f619
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-05 23:35:47 +0300

    creates role entity

---
+++ src/main/java/com/workintech/ecommerce/entity/Role.java
@@ -0,0 +1,27 @@
+package com.workintech.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="role",schema="ecommerce")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    @Override
+    public String getAuthority() {
+        return code;
+    }
+}
