db8faef99d506a6fcc9ff4f9c4a0611fdbb45007
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-05 23:44:31 +0300

    creates RoleController

---
+++ src/main/java/com/workintech/ecommerce/controller/RoleController.java
@@ -0,0 +1,28 @@
+package com.workintech.ecommerce.controller;
+
+import com.workintech.ecommerce.dto.RoleResponse;
+import com.workintech.ecommerce.entity.Role;
+import com.workintech.ecommerce.service.RoleService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/roles")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    @GetMapping
+    public List<RoleResponse> findAll(){
+        return roleService.findAll();
+    }
+
+    @PostMapping
+    public RoleResponse save(@RequestBody Role role){
+        return roleService.save(role);
+    }
+
+}
