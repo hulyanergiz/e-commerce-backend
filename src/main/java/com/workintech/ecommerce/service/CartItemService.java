88adf344cbaae9f3e3aac73f38ea9a5bd087b78c
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-08 23:37:53 +0300

    creates CartItemService interface

---
+++ src/main/java/com/workintech/ecommerce/service/CartItemService.java
@@ -0,0 +1,18 @@
+package com.workintech.ecommerce.service;
+
+import com.workintech.ecommerce.dto.CartItemResponse;
+import com.workintech.ecommerce.entity.CartItem;
+
+import java.util.List;
+
+public interface CartItemService {
+
+    List<CartItemResponse> findAllForUser(Long userId);
+
+    CartItem findById(Long id);
+
+    CartItemResponse getById(Long id);
+
+    CartItemResponse save(Long userId,CartItem cartItem);
+    CartItemResponse delete(Long userId,Long id);
+}
