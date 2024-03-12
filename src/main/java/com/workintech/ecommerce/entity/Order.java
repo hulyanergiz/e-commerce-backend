cdb61c3d44c0f7f1e32436c66acaba556c17c6d1
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-12 23:51:09 +0300

    adds manyToOne entity relations to Order

--- src/main/java/com/workintech/ecommerce/entity/Order.java
+++ src/main/java/com/workintech/ecommerce/entity/Order.java
@@ -19,4 +19,17 @@ public class Order {
     private String orderDate;
 
     private Double price;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="address_id")
+    private Address address;
+
+    @ManyToOne
+    @JoinColumn(name="card_id")
+    private Card card;
+
 }
