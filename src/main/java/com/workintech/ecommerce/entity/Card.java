fadd72e1faaeb72dea2bf4a616fcb3aa90fdc5f6
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-12 23:36:00 +0300

    creates Card entity class

---
+++ src/main/java/com/workintech/ecommerce/entity/Card.java
@@ -0,0 +1,33 @@
+package com.workintech.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="card",schema = "ecommerce")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name_on_card")
+    private String nameOnCard;
+
+    @Column(name="card_no")
+    private String cardNo;
+
+    @Column(name="expire_month")
+    private Integer expireMonth;
+
+    @Column(name = "expire_year")
+    private Integer expireYear;
+
+    private Integer cvv;
+
+
+}
