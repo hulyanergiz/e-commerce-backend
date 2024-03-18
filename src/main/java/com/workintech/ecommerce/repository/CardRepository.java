3a7c3c4f34402f6c2d5c3b4487089a023a6a4642
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-19 00:05:04 +0300

    adds query method for user in CardRepository

--- src/main/java/com/workintech/ecommerce/repository/CardRepository.java
+++ src/main/java/com/workintech/ecommerce/repository/CardRepository.java
@@ -2,6 +2,12 @@ package com.workintech.ecommerce.repository;
 
 import com.workintech.ecommerce.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CardRepository extends JpaRepository<Card,Long> {
+
+    @Query("SELECT c FROM Card c WHERE c.user.id=:userId")
+    List<Card> findCardsByUserId(Long userId);
 }
