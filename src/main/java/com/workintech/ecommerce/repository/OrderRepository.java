ae2d687934054d95a2f9e4964c38e6f723b960aa
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-19 00:05:25 +0300

    adds query method for user in OrderRepository

--- src/main/java/com/workintech/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/workintech/ecommerce/repository/OrderRepository.java
@@ -2,6 +2,12 @@ package com.workintech.ecommerce.repository;
 
 import com.workintech.ecommerce.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
+
+    @Query("SELECT o FROM Order o WHERE o.user.id=:userId")
+    List<Order> findOrdersByUserId(Long userId);
 }
