d4edb4c2ee96936360dab00ad72bdbb05c486b04
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-18 22:04:28 +0300

    creates AddressResponse

---
+++ src/main/java/com/workintech/ecommerce/dto/AddressResponse.java
@@ -0,0 +1,4 @@
+package com.workintech.ecommerce.dto;
+
+public record AddressResponse (String name,String surname, String phone,String city,String district,String neighborhood,String addressDetails){
+}
