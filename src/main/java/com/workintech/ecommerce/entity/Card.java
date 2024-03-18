4dc22885b220ae1fcae8ee3eccb1c3a5cc1e0adb
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-18 22:29:57 +0300

    adds cardTitle column in card entity

--- src/main/java/com/workintech/ecommerce/entity/Card.java
+++ src/main/java/com/workintech/ecommerce/entity/Card.java
@@ -15,6 +15,9 @@ public class Card {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="card_title")
+    private String cardTitle;
+
     @Column(name="name_on_card")
     private String nameOnCard;
 
