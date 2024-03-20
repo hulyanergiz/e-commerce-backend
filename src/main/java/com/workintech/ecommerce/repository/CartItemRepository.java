75acbe13fe96dd3fab2730422ae5188ac2a9e24f
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-21 00:41:47 +0300

    adds findCarItemForUser method with JPQL in CartItemRepository

--- src/main/java/com/workintech/ecommerce/repository/CartItemRepository.java
+++ src/main/java/com/workintech/ecommerce/repository/CartItemRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
 
     @Query("SELECT c FROM CartItem c WHERE c.user.id=:userId")
     List<CartItem> findCartItemsByUserId(Long userId);
+
+    @Query("SELECT c FROM CartItem c WHERE c.user.id = :userId AND c.id=:id")
+    Optional<CartItem> findCardItemForUser(Long userId,Long id);
 }
+
