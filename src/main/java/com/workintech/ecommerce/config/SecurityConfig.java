7ba2412f8ac6cb6e8d2703de2ef90a6f47009d9a
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-28 22:54:55 +0300

    adds restTemplate bean in securityConfig

--- src/main/java/com/workintech/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/workintech/ecommerce/config/SecurityConfig.java
@@ -12,10 +12,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
